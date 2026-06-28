203435c1ec8480644498ec0fa073df1cbff002f6
Divyansh022006 <divyanshagarwal173@gmail.com>
2026-06-28 17:21:24 +0530

    Updated image upload

--- src/services/imageUpload.js
+++ src/services/imageUpload.js
@@ -2,10 +2,15 @@ export async function uploadImage(file) {
   const formData = new FormData();
 
   formData.append("file", file);
-  formData.append("upload_preset", "instagram_preset");
+  formData.append(
+    "upload_preset",
+    import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET
+  );
 
   const response = await fetch(
-    "https://api.cloudinary.com/v1_1/drbfalhks/image/upload",
+    `https://api.cloudinary.com/v1_1/${
+      import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
+    }/image/upload`,
     {
       method: "POST",
       body: formData,
